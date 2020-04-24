50044029fa438412beaac43747b1b6f79b3b2f94
Aulia Ikvanda Yoren <a.ikvanda1@gmail.com>
2020-04-24 15:22:21 +0700

    ganti about us

--- app/src/main/java/com/aikvanda/danuskuapps/About/AboutUsActivity.java
+++ app/src/main/java/com/aikvanda/danuskuapps/About/AboutUsActivity.java
@@ -23,104 +23,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class AboutUsActivity extends AppCompatActivity
-        implements LoaderCallbacks<List<AboutUs>> {
-    private static final String LOG_TAG = AboutUsActivity.class.getName();
+public class AboutUsActivity extends AppCompatActivity{
 
-    /** URL for earthquake data from the USGS dataset */
-    private static final String USGS_REQUEST_URL =
-            "https://api.myjson.com/bins/11fjkf";
-
-    /**
-     * Constant value for the earthquake loader ID. We can choose any integer.
-     * This really only comes into play if you're using multiple loaders.
-     */
-    private static final int EARTHQUAKE_LOADER_ID = 1;
-
-    /** Adapter for the list of earthquakes */
-    private AboutUsAdapter mAdapter;
-
-    /** TextView that is displayed when the list is empty */
-    private TextView mEmptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aboutus_activity);
 
-        // Find a reference to the {@link ListView} in the layout
-        ListView aboutusListView = (ListView) findViewById(R.id.list);
-
-        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
-        aboutusListView .setEmptyView(mEmptyStateTextView);
-
-        // Create a new adapter that takes an empty list of earthquakes as input
-        mAdapter = new AboutUsAdapter(this, new ArrayList<AboutUs>());
-
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        aboutusListView.setAdapter(mAdapter);
-
-
-        // Get a reference to the ConnectivityManager to check state of network connectivity
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        // Get details on the currently active default data network
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-        // If there is a network connection, fetch data
-        if (networkInfo != null && networkInfo.isConnected()) {
-            // Get a reference to the LoaderManager, in order to interact with loaders.
-            LoaderManager loaderManager = getLoaderManager();
-
-            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-            // because this activity implements the LoaderCallbacks interface).
-            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
-        } else {
-            // Otherwise, display error
-            // First, hide loading indicator so error message will be visible
-            View loadingIndicator = findViewById(R.id.loading_indicator);
-            loadingIndicator.setVisibility(View.GONE);
-
-            // Update empty state with no connection error message
-            mEmptyStateTextView.setText( " No Internet Connection ");
-        }
-    }
-
-
-
-
-    @Override
-    public Loader<List<AboutUs>> onCreateLoader(int i, Bundle bundle) {
-        // Create a new loader for the given URL
-        return new AboutUsLoader(this, USGS_REQUEST_URL);
-    }
-
-    @Override
-    public void onLoadFinished(Loader<List<AboutUs>> loader, List<AboutUs> aboutUs) {
-        // Hide loading indicator because the data has been loaded
-        View loadingIndicator = findViewById(R.id.loading_indicator);
-        loadingIndicator.setVisibility(View.GONE);
-
-        // Set empty state text to display "No earthquakes found."
-        mEmptyStateTextView.setText("Tidak Ada Koneksi Internet");
-
-        // Clear the adapter of previous earthquake data
-        //mAdapter.clear();
-
-        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
-        // data set. This will trigger the ListView to update.
-        if (aboutUs != null && !aboutUs.isEmpty()) {
-            mAdapter.addAll(aboutUs);
-
-        }
 
     }
 
-    @Override
-    public void onLoaderReset(Loader<List<AboutUs>> loader) {
-        mAdapter.clear();
-    }
 }
